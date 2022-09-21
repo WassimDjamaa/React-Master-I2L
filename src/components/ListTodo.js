1b7679b4fb8ed9b46f65467b3f45f8cf2657f6f9
Wassim DJAMAA <56625264+WassimDjamaa@users.noreply.github.com>
2022-09-21 09:51:31 +0200

    implements edit input and update

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FormTodo } from "./FormTodo";
+import Todo from "./Todo";
 
 export const ListTodo = () => {
     const [todos, setTodos] = useState([]);
@@ -12,8 +13,30 @@ export const ListTodo = () => {
         const newTodos = [todo, ...todos];
 
         setTodos(newTodos);
+    };
 
-        console.log(todo,...todos);
+    const completeTodo = (id) => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    };
+
+    const removeTodo = (id) => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removeArr);
+    };
+
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)) {
+            return;
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
     };
 
     
@@ -22,6 +45,12 @@ export const ListTodo = () => {
         <div>
             <h1>Liste des tâches</h1>
             <FormTodo onSubmit={addTodo} />
+            <Todo
+                todos={todos}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+                updateTodo={updateTodo}
+            />
         </div>
 
     );
